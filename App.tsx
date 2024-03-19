91699bba019f0cc6a5dc3cda2b8ec5d0784530b5
kartiks123 <kartiksengar1@gmail.com>
2024-03-19 17:36:33 +0530

    Pressable, Status Bar, Platform

--- App.tsx
+++ App.tsx
@@ -1,118 +1,63 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { View, StyleSheet, Pressable, Text, StatusBar, Button, Platform } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [hide, setHide] = useState(false);
+  const [barStyle, setBarStyle] = useState('default');
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <View style={styles.main}>
+      <Text style={{ fontSize: 30 }}>Platform : {Platform.OS}</Text>
+      {
+        Platform.OS == "android" ?
+          <View style={{ backgroundColor: 'green', height: 100, width: 100 }}></View>
+          :
+          <View style={{ backgroundColor: 'red', height: 100, width: 100 }}></View>
+      }
+      <Text style={styles.text}>Hey There</Text>
+      <Text style={{fontSize:20}}> {JSON.stringify(Platform.constants.reactNativeVersion.minor)}</Text>
+
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+        backgroundColor="orange"
+        barStyle={barStyle}
+        hidden={false}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      <Button title='Toggle Status Bar' onPress={() => setHide(!hide)} />
+      <Button title='Update Style' onPress={() => setBarStyle("dark-content")} />
+
+      <Pressable
+        // onPress={() => console.warn("Normal on Press")}
+        // onLongPress={() => { console.warn("long press") }}
+        onPressIn={() => { console.warn("press in") }}
+        onPressOut={() => { console.warn("press out") }}
+      >
+        <Text style={styles.pressableBtn}>Pressable</Text>
+      </Pressable>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  main: {
+    flex: 1,
+    justifyContent: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  pressableBtn: {
+    backgroundColor: 'blue',
+    color: "#fff",
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+    fontSize: 20,
+    textAlign: 'center',
+    shadowColor: '#000',
+    elevation: 5
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  text: {
+    color: Platform.OS == "android" ? "orange" : 'blue',
+    fontSize: 20
+  }
+})
 
 export default App;
